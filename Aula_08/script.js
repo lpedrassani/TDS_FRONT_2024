f265c4199472c5b2f076e16604973c6c1052d049
Leonardo <leopedrassani201@gmail.com>
2024-07-30 21:59:52 -0300

    Aula_10

--- Aula_08/script.js
+++ Aula_08/script.js
@@ -1,62 +1,35 @@
-var alunos = [
+const alunos = [
     {
-        nome: "Leonardo Leopardo",
-        idade: 25,
-        email: "therealcodmaster1998@gmail.com"
+        nome: "Thiago Araujo",
+        ra: "1146443"
     },
     {
-        nome: "Bruninho Rei Delas",
-        idade: 19,
-        email: "bruniinhslipknot@gmail.com"
+        nome: "Bruno",
+        ra: "2847378"
     },
     {
-        nome: "João Peido Feijão",
-        idade: 29,
-        email: "joaoloks8bugre@gmail.com"
+        nome: "Pedro",
+        ra: "3657856"
     },
     {
-        nome: "Larissa Albuquerque",
-        idade: 12,
-        email: "larissinhassanhada@outlook.com.br"
-    },
-    {
-        nome: "Dom Casporra",
-        idade: 35,
-        email: "flickmandoformiguinhasz@hotmail.com"
-    },
-    {
-        nome: "Maicaca fia do Baitaca",
-        idade: 40,
-        email: "bostadevaca123@gmail.com"
+        nome: "Diovana",
+        ra: "3570953"
     }
 ];
 
-const body = window.document.getElementsByTagName("body")[0];
+
+const body = document.getElementsByTagName("body")[0];
+
+const h1 = window.document.createElement("h1");
+
+h1.innerHTML = "Alunos";
 
 const ol = window.document.createElement("ol");
+const li = window.document.createElement("li");
 
+li.innerHTML = "Guilherme"
 
-alunos.forEach((item)=>{
-    const li = window.document.createElement("li");
-    // li.innerHTML = "Nome: " + item.nome + " idade: " + item.idade + " email: " + item.email;
-    li.innerHTML = `Nome:${item.nome} Idade:${item.idade} Email:${item.email}`
-    ol.appendChild(li);
-});
+ol.appendChild(li)
 
-body.appendChild(ol);
-
-console.log(alunos)
-
-function AdicionaAluno(){
-    const input = window.document.getElementById("cadastro");
-
-    alunos.push(
-        {
-            nome: input.value,
-            idade: 40,
-            email: "bostadevaca123@gmail.com"
-        }
-    );
-
-    console.log(alunos);
-}
+body.appendChild(h1);
+body.appendChild(ol)
---
+++ README.md
@@ -0,0 +1,10 @@
+CONFIGURAR O GIT BASH;
+LOGA NA CONTA GIT HUB;
+FAZER CLONE DO REPOSITORIO TDS_FRONT_2024;
+BAIXAR  Visual Studio Code e instalar;
+ABRIR PASTA NO Visual Studio Code;
+
+GIT STATUS
+GIT ADD * ou . ou nome_do_arquivo
+GIT COMMIT -m "Meu primeiro commit"
+GIT PUSH
